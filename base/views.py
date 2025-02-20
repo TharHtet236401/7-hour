46af05a1385a4ae99a466bfd9701f13760dd2db6
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 03:32:00 +0700

    Add Topic and Message models, update Room model with relationships

--- base/views.py
+++ base/views.py
@@ -8,13 +8,14 @@ rooms = [
     {'id': 3, 'name': 'Let\'s learn React'},
 ]
 
+from .models import Room
+
 def home(request):
-    return render(request, 'base/home.html', {'rooms': rooms})
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
