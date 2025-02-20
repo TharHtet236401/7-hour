aa0a9b1815ce53e8c572148aa59bfecdcb45f061
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 00:54:32 +0700

    Add templates for home and room views

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello from home page")
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse("Hello from room page")
+    return render(request, 'room.html')
