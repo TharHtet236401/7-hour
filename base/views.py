de8fc1ec2eaba579bca26375bb038ad8a38c16f3
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 01:17:35 +0700

    Implement dynamic room details display in views and template

--- base/views.py
+++ base/views.py
@@ -3,13 +3,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 rooms = [
-    {'id': 1, 'name': 'Room 1'},
-    {'id': 2, 'name': 'Room 2'},
-    {'id': 3, 'name': 'Room 3'},
+    {'id': 1, 'name': 'Let\'s learn Python'},
+    {'id': 2, 'name': 'Let\'s learn Django'},
+    {'id': 3, 'name': 'Let\'s learn React'},
 ]
 
 def home(request):
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request,pk):
-    return render(request, 'base/room.html')
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
