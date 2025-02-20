daad9a75a28c40d5f2c911ce2a9152a2d586dd68
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 03:59:19 +0700

    Add ordering to Room model for consistent display

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
-        
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
@@ -16,6 +16,9 @@ class Room(models.Model):
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
