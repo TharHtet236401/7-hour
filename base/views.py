3338cc1dfd3710d99bd0ba5781d0c4355a1e2b59
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 01:02:27 +0700

    Add rooms list to home view and template

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
