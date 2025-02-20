28ca72c4d39e4d5cf5f7785de160fee1fd25a7dc
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 03:56:23 +0700

    Implement form-based room creation with Django forms

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
-
+from .forms import RoomForm
 rooms = [
     {'id': 1, 'name': 'Let\'s learn Python'},
     {'id': 2, 'name': 'Let\'s learn Django'},
@@ -21,7 +21,13 @@ def room(request,pk):
     return render(request, 'base/room.html', context)
 
 def createRoom(request):
-    context = {}
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
 
