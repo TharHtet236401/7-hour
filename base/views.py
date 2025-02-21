7384a05bfc9f97143108e1015ba3a6580373e363
TharHtet236401 <tharhteta3@gmail.com>
2025-02-21 16:00:31 +0700

    Add login page route and view

--- base/views.py
+++ base/views.py
@@ -13,6 +13,10 @@ rooms = [
 
 from .models import Room
 
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html',context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
